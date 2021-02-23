19ddcb082c6044d54f3adfe1290a1434ad829a6e
Klaas Cuvelier <klaas@showpad.com>
2021-02-23 10:32:48 +0100

    basic setup script

---
+++ setup.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+function setup {
+    # update to latest dotfiles
+    git pull origin main
+    
+    # install XCode CLI tools if needed
+    if ! xcode-select -p > /dev/null; then
+        xcode-select --install
+    fi
+
+    # backup home directory dotfiles
+    rsync -a --progress --no-perms --exclude ".git/" --exclude ".DS_Store" ~/.[^.]* ~/home-backup
+
+    # symlink only root dotfiles into home directory
+    for item in $(pwd)/.[^.]*; do
+        # Skip .git, .DS_Store, .macos
+        if [[ $item == */.git ]] || [[ $item == */.DS_Store ]] || [[ $item == */.macos ]]; then
+            continue
+        fi
+
+        # create the symlink
+        dest="$HOME/$(basename $item)"
+        ln -sfnv $item $dest 
+    done
+
+    # Run setup for all tools (comment out what you don't need or want)
+    # ( cd homebrew && ./setup.sh )
+    # ( cd fish && ./setup.sh )
+    # ( cd iterm && ./setup.sh )
+    # ( cd vscode && ./setup.sh )
+}
+
+# get user confirmation
+read -p "This may overwrite existing files in your home directory. Are you sure? (y/N) " -n 1;
+echo "";
+
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    setup
+fi;
